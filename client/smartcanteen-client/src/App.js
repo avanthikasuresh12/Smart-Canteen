fe2200f3865c17c916d27beb369f0812cfa82c95
akkuvy <vyshnuakku@gmail.com>
2023-02-04 22:11:41 +0530

    hotel registration ui added

--- client/smartcanteen-client/src/App.js
+++ client/smartcanteen-client/src/App.js
@@ -1,33 +1,41 @@
- import React from 'react';
+ import * as React from 'react';
 import './App.css';
 import {
   createBrowserRouter,
   RouterProvider,
+  Route,Routes
 } from "react-router-dom";
 
  
-import "bootstrap/dist/css/bootstrap.min.css"
 
- import Login from "./pages/login"
- import Home from "./pages/home"
- import Footer from "./components/footer"
- import NavBar from './components/navbar';
+import { Container  } from "react-bootstrap";
 
+import RegisterHotel  from './pages/SuperAdmin/RegisterHotel';
+ import Home from "./pages/Home"
+ import Footer from "./components/Footer"
+ import NavBar from './components/Navbar';
+ 
 const router = createBrowserRouter([
   {
   
-    path: "/login",
-    element: <Login/>,
+    path: "superadmin/add-hoteladmin",
+    element:   <Container>
+ 
+        <RegisterHotel />
+     
+  </Container>,
   }
   ,{
     path:"/",
     element:<div><NavBar/>
     <Home/>
     <Footer/></div>,
+   
   }
 ]);
 function App() {
   return (
+    
     <div >
        <RouterProvider router={router}></RouterProvider>
     </div>
---
+++ client/smartcanteen-client/src/pages/SuperAdmin/RegisterHotel.jsx
@@ -0,0 +1,207 @@
+import React, { useState } from "react";
+
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+ import Select from 'react-select'
+import {
+  MDBBtn,
+  MDBContainer,
+  MDBRow,
+  MDBCol,
+  MDBCard,
+  MDBCardBody,
+  MDBInput,
+  MDBCheckbox
+}
+from 'mdb-react-ui-kit';
+ import "./RegisterHotel.css"
+
+const RegisterHotel=()=>{
+  //usestate 
+  const [RestaurantName, setRestaurantName] = useState("");
+  const [adminName,setAdminName]=useState("");
+  const [category,setCategory]=useState("");
+  const [phone,setPhone]=useState("");
+  const [email,setEmail] =useState("");
+  const [Password,setPassword]=useState("");
+   const [street,setStreet]=useState("");
+     const [zipCode,setZipCode]=useState("");
+   const [state,setState]=useState("");
+  const [country,setCountry]=useState("");
+  const [district,setDistrict]=useState("");
+
+  
+const options = [
+  { value: 'chocolate', label: 'Chocolate' },
+  { value: 'strawberry', label: 'Strawberry' },
+  { value: 'vanilla', label: 'Vanilla' }
+]
+function HandleCateogoryChange(e){
+  setCategory(e.target.value)
+}
+    return(
+      <MDBContainer fluid className='h-custom'>
+
+      <MDBRow className='d-flex justify-content-center align-items-center h-100'>
+        <MDBCol col='12' className='m-5'>
+
+          <MDBCard className='card-registration card-registration-2' style={{borderRadius: '15px'}}>
+
+            <MDBCardBody className='p-0'>
+
+              <MDBRow>
+
+                <MDBCol md='6' className='p-5 bg-white'>
+
+                  <h3 className="fw-normal mb-5" style={{color: '#4835d4'}}>Hotel Infomation</h3>
+              
+{/* //FisrtName Section */}
+                  <MDBRow>
+
+                    <MDBCol md='6'>
+                      <MDBInput wrapperClass='mb-4' label='Restaurant Name' size='lg' id='form1' type='text'value={RestaurantName} 
+                      
+                      onChange={(e)=>{
+                        setRestaurantName(e.target.value)
+                      }}/>
+{/* 
+                      last name section */}
+                    </MDBCol>
+
+                    <MDBCol md='6'>
+                      <MDBInput wrapperClass='mb-4' label='Restaurant Admin Name' size='lg' id='form2' type='text' value={adminName}
+                      onChange={(e)=>{
+                        setAdminName(e.target.value)
+                      }}
+                      />
+                    </MDBCol>
+
+                  </MDBRow>
+
+                   <MDBRow md="6">
+                    <MDBCol>
+                     
+                    <Select options={options} 
+                    value ={category}
+                    onChange={(e)=>{
+                      setCategory(e)
+                    }} />
+                    <span>
+                        Category
+                      </span>
+                    </MDBCol>
+                   </MDBRow>
+  
+                
+
+                  <MDBRow>
+
+                    <MDBCol md='6'>
+                      <MDBInput wrapperClass='mt-4' label='Phone Number' size='lg' id='form4' type='text' 
+                      value={phone}
+                      onChange={(e)=>{
+                        setPhone(e.target.value)
+                      }}/>
+                    </MDBCol>
+                 
+<MDBCol md='6'>
+  <MDBInput wrapperClass='mt-4' label='Email ' size='lg' id='form4' type='Email'
+  value={email}
+  onChange={(e)=>{
+    setEmail(e.target.value)
+  }}/>
+</MDBCol>
+                       
+
+
+
+
+
+
+</MDBRow>
+<MDBRow>
+<MDBCol md='6'>
+  <MDBInput wrapperClass='mt-4' label='Password ' size='lg' id='form4' type='password'
+  value={Password}
+  onChange={(e)=>{
+    setPassword(e.target.value)
+  }}/>
+</MDBCol>
+
+
+
+</MDBRow>
+                </MDBCol>
+
+
+                <MDBCol md='6' className='bg-indigo p-5'>
+
+                  <h3 className="fw-normal mb-5 text-white" style={{color: '#4835d4'}}>Address Details</h3>
+                  <MDBInput wrapperClass='mb-4' labelClass='text-white' label='Street + Nr' size='lg' id='form5' type='text'
+                  value={street}
+                  onChange={(e)=>{
+                    setStreet(e.target.value)
+                  }}/>
+                 
+                  <MDBRow>
+
+                    <MDBCol md='5'>
+                      <MDBInput wrapperClass='mb-4' labelClass='text-white' label='District' size='lg' id='form6' type='text'
+                      value={district}
+                      onChange={(e)=>{
+                        setDistrict(e.target.value)
+                      }}
+                      />
+                    </MDBCol>
+
+                    <MDBCol md='7'>
+                      <MDBInput wrapperClass='mb-4' labelClass='text-white' label='State' size='lg' id='form7' type='text'
+                      value={state}
+                      onChange={(e)=>{
+                        setState(e.target.value)
+                      }}
+                      />
+                    </MDBCol>
+
+                  </MDBRow>
+
+                  <MDBInput wrapperClass='mb-4' labelClass='text-white' label='Country' size='lg' id='form8' type='text'
+                  value={country}
+                  onChange={(e)=>{
+                    setCountry(e.target.value)
+                  }}
+                  />
+
+<MDBRow>
+<MDBCol md='6'>
+  <MDBInput  wrapperClass='mb-4' labelClass='text-white' label='Zip Code' size='lg' id='form8' type='text'
+  value={zipCode}
+  onChange={(e)=>{
+    setZipCode(e.target.value)
+  }}/>
+</MDBCol>
+
+
+
+</MDBRow>
+
+                  
+                  <MDBCheckbox name='flexCheck' id='flexCheckDefault' labelClass='text-white mb-4' label='I do accept the Terms and Conditions of your site.' />
+                  <MDBBtn color='light' size='lg'>Register</MDBBtn>
+
+                </MDBCol>
+              </MDBRow>
+
+            </MDBCardBody>
+
+          </MDBCard>
+
+        </MDBCol>
+      </MDBRow>
+
+    </MDBContainer>
+
+    )
+}
+
+export default RegisterHotel;
